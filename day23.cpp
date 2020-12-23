75fd494b0290f78c129d60f33faae22d92839b24
Alejandro Alfonso <aalfonso@cern.ch>
2020-12-23 19:29:54 +0100

    Solved second half of day 23

--- day23.cpp
+++ day23.cpp
@@ -2,141 +2,129 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <list>
+#include <map>
+#include <vector>
 
-using Cups = std::list<size_t>;
+using Cups = std::pair<std::map<size_t, size_t>, size_t>;
 
-void Print(Cups const &cups)
+void Print(Cups &cups, size_t N = 1)
 {
-    for (auto cup : cups)
-        std::cout << "(" << cup << ")";
+    for (size_t i = 0; i < cups.first.size(); i++)
+    {
+        std::cout << cups.first[N];
+        N = cups.first[N];
+    }
     std::cout << std::endl;
 }
 
-Cups StringToCups(std::string const &str)
+Cups StringToCups(std::string const &str, size_t const size)
 {
     Cups cups;
-    for (auto c : str)
-        cups.push_back(c - 48);
-    return cups;
-}
-
-Cups ReadCups(std::string const &filename)
-{
-    Cups cups;
-    std::ifstream infile;
-    infile.open(filename.c_str());
-    if (!infile.is_open())
-        return cups;
-
-    // Just one line, transform string into digits
-    std::string input_thing;
-    std::getline(infile, input_thing);
-    cups = StringToCups(input_thing);
-    infile.close();
-    return cups;
-}
-
-void Fill(Cups &cups, size_t const size)
-{
-    while (cups.size() < size)
-        cups.push_back(cups.size() + 1);
-}
-
-Cups ReadCups2(std::string const &filename, size_t const size)
-{
-    Cups cups;
-    std::ifstream infile;
-    infile.open(filename.c_str());
-    if (!infile.is_open())
-        return cups;
-
-    // Just one line, transform string into digits
-    std::string input_thing;
-    std::getline(infile, input_thing);
-    cups = StringToCups(input_thing);
-    infile.close();
-    Fill(cups, size);
-    return cups;
-}
-
-Cups::iterator FindNextCup(Cups &cups, uint32_t const N_cupsmoved)
-{
-    size_t search_index = *cups.begin() - 1;
-    if (search_index == 0)
-        search_index = uint32_t(cups.size()) + N_cupsmoved;
-    //We loop until we find a match. There's always a match
-    while (true)
+    for (size_t i = 0; i < size - 1; i++)
     {
-        auto cup = std::find(cups.begin(), cups.end(), search_index);
-        if (cup != cups.end())
-        {
-            std::advance(cup, 1);
-            return cup;
-        }
+        if (i < str.size() - 1)
+            cups.first[str[i] - 48] = str[i + 1] - 48;
+        else if (i == str.size() - 1)
+            cups.first[str[i] - 48] = str.size() + 1;
+        else
+            cups.first[i + 1] = i + 2;
+    }
+    cups.first[size] = str[0] - 48;
+    if (cups.first.size() > size)
+        cups.second = str[str.size() - 1] - 48;
+    else
+        cups.second = size;
+
+    return cups;
+}
+
+Cups ReadCups(std::string const &filename, size_t size = 0)
+{
+    Cups cups;
+    std::ifstream infile;
+    infile.open(filename.c_str());
+    if (!infile.is_open())
+        return cups;
+
+    // Just one line, transform string into digits
+    std::string input_thing;
+    std::getline(infile, input_thing);
+    if (size == 0)
+        size = input_thing.size();
+    cups = StringToCups(input_thing, size);
+    infile.close();
+    return cups;
+}
+
+size_t GetNBelow(size_t const &N, std::vector<size_t> const &moving_cups, size_t const &size)
+{
+    size_t search_index = N - 1;
+    if (search_index == 0)
+        search_index = size;
+    for (size_t i = 0; i < moving_cups.size(); i++)
+    {
+        auto cup = std::find(moving_cups.begin(), moving_cups.end(), search_index);
+        if (cup == moving_cups.end())
+            return search_index;
 
         search_index--;
         if (search_index == 0)
-            search_index = uint32_t(cups.size()) + N_cupsmoved;
+            search_index = size;
     }
+    return search_index;
 }
 
 void PlayCups(Cups &cups, uint32_t const N_cupsmoved)
 {
-    //Play 1 round of cups
-    //Our current cup is ALWAYS the first. We are going to move N_cupsmoved cups
-    //Extract these N cups
-    Cups::iterator begin_it = cups.begin();
-    Cups::iterator target_it = cups.begin();
-    std::advance(begin_it, 1);
-    std::advance(target_it, N_cupsmoved + 1);
-
-    Cups extracted_cups;
-    extracted_cups.splice(extracted_cups.begin(), cups, begin_it, target_it);
-
-    //Now look for cup where these N will be inserted
-    target_it = FindNextCup(cups, N_cupsmoved);
-    //Reinsert at that place
-    cups.splice(target_it, extracted_cups, extracted_cups.begin(), extracted_cups.end());
-
-    //Last, put first element to last position
-    target_it = cups.begin();
-    std::advance(target_it, 1);
-    std::rotate(cups.begin(), target_it, cups.end());
+    //So for any Number of moved cups, we must do several changes
+    //The next element to be played with is N moves away, let's get it and save it for later
+    size_t new_first = cups.first[cups.second];
+    size_t new_last = new_first;
+    std::vector<size_t> moving_cups;
+    for (size_t i = 0; i < N_cupsmoved; i++)
+    {
+        new_first = cups.first[new_first];
+        moving_cups.push_back(new_first);
+    }
+    new_first = cups.first[new_first];
+    size_t N_below = GetNBelow(new_last, moving_cups, cups.first.size());
+    //We need to change 4 numbers in the map:
+    //1) The last element being moved will now point to the value our N-1 was previously pointing to
+    cups.first[*moving_cups.rbegin()] = cups.first[N_below];
+    //2) The N-1(or whatever) entry number will now contain the first value being moved
+    cups.first[N_below] = moving_cups[0];
+    //3) The last element will now point to the (old) first number
+    //4) The (old) first element will now point to the element 4 moves away
+    if (cups.second == N_below)
+    {
+        cups.first[cups.second] = *moving_cups.begin();
+        cups.first[*moving_cups.rbegin()] = new_last;
+    }
+    else
+    {
+        cups.first[cups.second] = new_last;
+    }
+    cups.first[new_last] = new_first;
+    cups.second = new_last;
     return;
 }
 
-void PlayCups(Cups &cups, uint32_t const N_rounds, uint32_t const N_cupsmoved)
+void PlayCups(Cups &cups, size_t const N_rounds, uint32_t const N_cupsmoved)
 {
     //Play N rounds
-    for (uint32_t i = 0; i < N_rounds; i++)
+    for (size_t i = 0; i < N_rounds; i++)
     {
-        if (i % 1000 == 0)
-            std::cout << i << std::endl;
         PlayCups(cups, N_cupsmoved);
     }
 }
 
-void Reorder(Cups &cups, uint32_t number)
-{
-    //Find the 1 in the vector
-    Cups::iterator target_it;
-    for (target_it = cups.begin(); *target_it != number; ++target_it)
-    {
-    }
-
-    //Rotate
-    std::rotate(cups.begin(), target_it, cups.end());
-}
-
-uint64_t GetProduct(Cups const &cups, size_t const N)
+uint64_t GetProduct(Cups &cups, size_t const N, size_t start)
 {
     uint64_t result = 1;
-    Cups::const_iterator it = cups.begin();
     for (int i = 0; i < N; i++)
     {
-        std::advance(it, 1);
-        result = result * (*it);
+        result = result * cups.first[start];
+        start = cups.first[start];
     }
     return result;
 }
@@ -147,18 +135,16 @@ int main()
     auto cups = ReadCups("C:\\Users\\alex1\\git\\AdventOfCode\\day23\\input.txt");
     //Play 100 rounds
     PlayCups(cups, 100, 3);
-    //Reorder putting 1 in first place
-    Reorder(cups, 1);
 
     std::cout << "Result: ";
-    Print(cups);
+    Print(cups, 1);
 
     //Part two, read the cups in a different manner and replay the game
-    cups = ReadCups2("C:\\Users\\alex1\\git\\AdventOfCode\\day23\\input.txt", 1000000);
-    //Now play 10^7 moves
+    cups = ReadCups("C:\\Users\\alex1\\git\\AdventOfCode\\day23\\input.txt", 1000000);
+    //Play 10^7 rounds
     PlayCups(cups, 10000000, 3);
-    Reorder(cups, 1);
-    auto result2 = GetProduct(cups, 2);
+
+    auto result2 = GetProduct(cups, 2, 1);
     std::cout << "Result2: " << result2 << std::endl;
 
     return 0;
