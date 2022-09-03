55c3dc2dae3499e1032a98ee32e0d6e468dd8c0f
Alejandro Alfonso <alex.1993.24@gmail.com>
2022-09-03 09:22:20 +0200

    Update FileUtils

--- 2021/include/FileUtils.h
+++ 2021/include/FileUtils.h
@@ -4,24 +4,37 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <list>
 
+template<typename T> T CreateContainerFromFile( const std::string& aFileName );
 template<typename T> typename std::vector<T> CreateVectorFromFile( const std::string& aFileName );
+template<typename T> typename std::list<T> CreateListFromFile( const std::string& aFileName );
 
-template<typename T> typename std::vector<T> CreateVectorFromFile( const std::string& aFileName )
+template<typename T> T CreateContainerFromFile( const std::string& aFileName )
 {
-	std::vector<T> result;
+	T result;
 
 	std::ifstream infile;
 	infile.open( aFileName );
 	if( infile )
 	{
-		T input;
+		typename T::value_type input;
 		while( infile >> input )
 			result.emplace_back( std::move( input ) );
 	}
 	infile.close();
 
 	return result;
+}
+
+template<typename T> typename std::vector<T> CreateVectorFromFile( const std::string& aFileName )
+{
+	return CreateContainerFromFile<std::vector<T>>( aFileName );
+}
+
+template<typename T> typename std::list<T> CreateListFromFile( const std::string& aFileName )
+{
+	return CreateContainerFromFile<std::list<T>>( aFileName );
 }
 
 #endif // FILE_UTILS_H
