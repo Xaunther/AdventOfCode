2ddfaee6c1b746e4bc9289935ec21481737f6547
Alejandro Alfonso <alex.1993.24@gmail.com>
2022-09-03 09:46:10 +0200

    Solved part 2

--- 2021/day3/src/main.cpp
+++ 2021/day3/src/main.cpp
@@ -17,6 +17,10 @@ static const std::string EXAMPLE_FILE_NAME = "../../../../"s + YEAR + "/"s + DAY
 
 unsigned long Part1( const std::string& aFileName );
 unsigned long Part2( const std::string& aFileName );
+template<typename T> std::vector<unsigned int> CreateSumVector( const T& aBinaries );
+unsigned int CalculateOxygenRating( const std::list<CBinary>& aBinaries );
+unsigned int CalculateCO2Rating( const std::list<CBinary>& aBinaries );
+unsigned int CalculateRating( std::list<CBinary> aBinaries, const CBinary::E_RATE& aRate );
 
 }
 
@@ -35,16 +39,48 @@ namespace
 unsigned long Part1( const std::string& aFileName )
 {
 	const auto& binaries = CreateVectorFromFile<CBinary>( aFileName );
-	std::vector<unsigned int> sumVector( binaries.front().GetBinaryNumber().size() );
-	for( const auto& binary : binaries )
-		for( size_t index = 0;index < binary.GetBinaryNumber().size(); ++index )
-			sumVector[ index ] += static_cast< unsigned int >( binary.GetBinaryNumber()[ index ] );
+	const auto& sumVector = CreateSumVector( binaries );
 	return CBinary{ sumVector, binaries.size(), CBinary::E_RATE::GAMMA }.CalculateDecimalNumber() * CBinary { sumVector, binaries.size(), CBinary::E_RATE::EPSILON }.CalculateDecimalNumber();
 }
 
 unsigned long Part2( const std::string& aFileName )
 {
-	return 0;
+	auto binaries = CreateListFromFile<CBinary>( aFileName );
+	return CalculateOxygenRating( binaries ) * CalculateCO2Rating( binaries );
 }
+
+template<typename T> std::vector<unsigned int> CreateSumVector( const T& aBinaries )
+{
+	std::vector<unsigned int> result( aBinaries.front().GetBinaryNumber().size() );
+	for( const auto& binary : aBinaries )
+		for( size_t index = 0;index < binary.GetBinaryNumber().size(); ++index )
+			result[ index ] += static_cast< unsigned int >( binary.GetBinaryNumber()[ index ] );
+
+	return result;
+}
+
+unsigned int CalculateOxygenRating( const std::list<CBinary>& aBinaries )
+{
+	return CalculateRating( aBinaries, CBinary::E_RATE::GAMMA );
+}
+
+unsigned int CalculateCO2Rating( const std::list<CBinary>& aBinaries )
+{
+	return CalculateRating( aBinaries, CBinary::E_RATE::EPSILON );
+}
+
+unsigned int CalculateRating( std::list<CBinary> aBinaries, const CBinary::E_RATE& aRate )
+{
+	size_t searchIndex = 0;
+	while( aBinaries.size() > 1 )
+	{
+		const auto& sumVector = CreateSumVector( aBinaries );
+		const auto& keptDigit = CBinary( sumVector, aBinaries.size(), aRate ).GetBinaryNumber()[ searchIndex ];
+		aBinaries.remove_if( [&keptDigit, &searchIndex]( const CBinary& aBinary ) { return aBinary.GetBinaryNumber()[ searchIndex ] != keptDigit; } );
+		++searchIndex;
+	}
+	return aBinaries.front().CalculateDecimalNumber();
+}
+
 
 }
