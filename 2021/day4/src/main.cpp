3123ed220ec6882695aaa4ef59d3ab6b67c7745c
Alejandro Alfonso <alex.1993.24@gmail.com>
2022-09-05 07:51:37 +0200

    Prepare day 4

---
+++ 2021/day4/src/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+#include "year.h"
+#include "day.h"
+
+#include "FileUtils.h"
+
+namespace
+{
+
+using namespace std::literals;
+
+static const std::string INPUT_FILE_NAME = "../../../../"s + YEAR + "/"s + DAY + "/input.txt"s;
+static const std::string EXAMPLE_FILE_NAME = "../../../../"s + YEAR + "/"s + DAY + "/example.txt"s;
+
+unsigned long Part1( const std::string& aFileName );
+unsigned long Part2( const std::string& aFileName );
+
+}
+
+int main()
+{
+	std::cout << "Part 1 example: " << Part1( EXAMPLE_FILE_NAME ) << std::endl;
+	std::cout << "Part 1 solution: " << Part1( INPUT_FILE_NAME ) << std::endl;
+	std::cout << "Part 2 example: " << Part2( EXAMPLE_FILE_NAME ) << std::endl;
+	std::cout << "Part 2 solution: " << Part2( INPUT_FILE_NAME ) << std::endl;
+	return 0;
+}
+
+namespace
+{
+
+unsigned long Part1( const std::string& aFileName )
+{
+	return 0;
+}
+
+unsigned long Part2( const std::string& aFileName )
+{
+	return 0;
+}
+
+}
