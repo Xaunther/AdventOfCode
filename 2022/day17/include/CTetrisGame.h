1d53e405d5e26849f5201df4afd61bb2464dc6ef
Alejandro Alfonso <alex.1993.24@gmail.com>
2022-12-20 08:48:41 +0100

    Solve day 17 part 1

--- 2022/day17/include/CTetrisGame.h
+++ 2022/day17/include/CTetrisGame.h
@@ -9,14 +9,16 @@
 #include "CWinds.h"
 
 #include <array>
-#include <map>
+#include <deque>
 
 class CTetrisGame
 {
 public:
+	static const std::size_t WIDTH = 7;
+
 	using piece_sequence = std::array<std::reference_wrapper<ATetrisPiece>, 5>;
-	using rocks_row = std::array<bool, 7>;
-	using rocks = std::map<std::size_t, rocks_row, std::greater<std::size_t>>;
+	using rocks_row = std::array<bool, WIDTH>;
+	using rocks = std::deque<std::pair<std::size_t, rocks_row>>;
 
 	explicit CTetrisGame( const CWinds& aWinds, const std::size_t& aNumGames );
 
@@ -26,6 +28,8 @@ public:
 	std::size_t GetHeight() const;
 
 private:
+	void AddPieceToRocks( const ATetrisPiece& aPiece );
+
 	CHorizontalPiece mHorizontalPiece;
 	CTruenoPiece mTruenoPiece;
 	CCornerPiece mCornerPiece;
@@ -34,6 +38,6 @@ private:
 	piece_sequence mPieceSequence;
 
 	const CWinds::winds mWinds;
-	const std::size_t mRightBorder = 8;
+	const std::size_t mRightBorder = WIDTH + 1;
 	rocks mRocks;
 };
--- 2022/day17/src/CTetrisGame.cpp
+++ 2022/day17/src/CTetrisGame.cpp
@@ -1,5 +1,7 @@
 #include "CTetrisGame.h"
 
+#include <algorithm>
+#include <iostream>
 namespace
 {
 
@@ -18,6 +20,7 @@ CTetrisGame::CTetrisGame( const CWinds& aWinds, const std::size_t& aNumGames ):
 
 void CTetrisGame::Play( const std::size_t& aNumGames )
 {
+	auto windIt = mWinds.cbegin();
 	for( std::size_t gameCount = 0; gameCount < aNumGames; ++gameCount )
 	{
 		// Select our piece and set its initial position
@@ -28,10 +31,42 @@ void CTetrisGame::Play( const std::size_t& aNumGames )
 		while( true )
 		{
 			// Moved by the wind
+			if( !( *windIt ) ) // Left
+			{
+				if( CanMoveLeft( piece, mRocks, rockIt ) )
+					piece.MoveLeft();
+			}
+			else //Right
+				if( CanMoveRight( piece, mRocks, rockIt ) )
+					piece.MoveRight();
+			++windIt;
+			if( windIt == mWinds.cend() )
+				windIt = mWinds.cbegin();
+
 			// Drop down
+			if( CanMoveDown( piece, mRocks, rockIt ) )
+				piece.MoveDown();
+			else
+				break;
+
+			//Move iterator to the first rock that may interact with the falling piece
+			if( rockIt != mRocks.cend() && ( *rockIt ).first > piece.GetTopPosition() )
+				++rockIt;
 		}
-		// Add rocks to floor and remove tiles below a completed line
+		// Add rocks to floor and remove rows below a completed line
+		AddPieceToRocks( piece );
 	}
+	/*
+	std::cout << std::endl;
+	for( const auto& rockRow : mRocks )
+	{
+		std::cout << "|";
+		for( const auto& rock : rockRow.second )
+			std::cout << ( rock ? '#' : '.' );
+		std::cout << "|\n";
+	}
+	std::cout << "+-------+\n";
+	*/
 }
 
 const CWinds::winds& CTetrisGame::GetWinds() const
@@ -44,6 +79,30 @@ std::size_t CTetrisGame::GetHeight() const
 	return mRocks.empty() ? 0 : ( *mRocks.begin() ).first;
 }
 
+void CTetrisGame::AddPieceToRocks( const ATetrisPiece& aPiece )
+{
+	//Add new heights
+	if( GetHeight() < aPiece.GetTopPosition() )
+	{
+		for( std::size_t height = GetHeight() + 1; height <= aPiece.GetTopPosition(); ++height )
+			mRocks.emplace_front( height, rocks::value_type::second_type{} );
+	}
+
+	const auto& height = GetHeight();
+
+	//Add piece
+	for( const auto& pos : aPiece.GetPositions() )
+	{
+		if( height - pos.second < mRocks.size() )
+		{
+			auto& rockRow = mRocks[ height - pos.second ].second;
+			rockRow[ pos.first - 1 ] = true;
+			if( std::ranges::all_of( rockRow, []( auto&& aRock ) { return aRock; } ) )
+				mRocks.erase( mRocks.begin() + height - pos.second + 1, mRocks.end() );
+		}
+	}
+}
+
 namespace
 {
 
@@ -54,18 +113,36 @@ bool CanMoveLeft( const ATetrisPiece& aTetrisPiece, const CTetrisGame::rocks& aR
 	for( const auto& newPos : aTetrisPiece.ProjectLeftPositions() )
 	{
 		const auto& rowFound = std::find_if( aRockHint, aRocks.cend(), [ &newPos ]( auto&& aRockRow ) { return aRockRow.first == newPos.second; } );
+		if( rowFound != aRocks.cend() && ( *rowFound ).second[ newPos.first - 1 ] )
+			return false;
 	}
+	return true;
 }
 
 bool CanMoveRight( const ATetrisPiece& aTetrisPiece, const CTetrisGame::rocks& aRocks, const CTetrisGame::rocks::const_iterator& aRockHint )
 {
-
+	if( aTetrisPiece.GetRightmostPosition() == CTetrisGame::WIDTH )
+		return false;
+	for( const auto& newPos : aTetrisPiece.ProjectRightPositions() )
+	{
+		const auto& rowFound = std::find_if( aRockHint, aRocks.cend(), [ &newPos ]( auto&& aRockRow ) { return aRockRow.first == newPos.second; } );
+		if( rowFound != aRocks.cend() && ( *rowFound ).second[ newPos.first - 1 ] )
+			return false;
+	}
+	return true;
 }
 
 bool CanMoveDown( const ATetrisPiece& aTetrisPiece, const CTetrisGame::rocks& aRocks, const CTetrisGame::rocks::const_iterator& aRockHint )
 {
-
+	if( aTetrisPiece.GetBottomPosition() == 1 )
+		return false;
+	for( const auto& newPos : aTetrisPiece.ProjectDownPositions() )
+	{
+		const auto& rowFound = std::find_if( aRockHint, aRocks.cend(), [ &newPos ]( auto&& aRockRow ) { return aRockRow.first == newPos.second; } );
+		if( rowFound != aRocks.cend() && ( *rowFound ).second[ newPos.first - 1 ] )
+			return false;
+	}
+	return true;
 }
-
 
 }
